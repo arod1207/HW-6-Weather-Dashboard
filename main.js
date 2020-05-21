89ae1597a7a5cfc906e69284970e456d20ba5b37
Arod1207 <arod1207@me.com>
2020-05-21 17:46:44 -0500

    5 day forcast working, need to work on images

--- main.js
+++ main.js
@@ -21,33 +21,23 @@ $.ajax({
     var windSpeed = response.wind.speed;
     var lon = response.coord.lon;
     var lat = response.coord.lat;
-    var weather = response.weather[0].main;
     var weatherIcon = response.weather[0].icon
     var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
  
-    var newImg = $('<img>');
-    // var newDiv = $('<div>');
     
-    console.log(iconURL)
-    
-    $("#cityName").html(city)
+    $("#cityName").html(city.value)
     $("#currentWeatherImg").append('src' , iconURL)
     $("#temperature").text(`Temperature: ${temp} \xB0F`);
     $("#humidity").text(`Humidty: ${humidity} %`)
     $("#windSpeed").text(`Wind Speed: ${windSpeed} MPH`)
     
- 
-
+    var newImg = $('<img>');
     newImg.attr('src', iconURL);
      $('#cityName').append(newImg)
-    // $('body').append(newDiv);
-    // newDiv.append(p)
-    // newDiv.append(weather);
-    // newDiv.append(newImg);
+   
 
 // Api for UV Index //
     var apiUvIndexUrl = apiUvIndex +`lat=${lat}`+`&lon=${lon}`+ apiKey;
-    console.log(apiUvIndexUrl)
 })
 
 // api for the 5 day forcast //
@@ -57,5 +47,48 @@ $.ajax({
     method: 'GET',
 }).then(function(fResponse){
     console.log(fResponse);
+    var day1day = fResponse.list[0].dt_txt;
+    var day2day = fResponse.list[8].dt_txt;
+    var day3day = fResponse.list[16].dt_txt;
+    var day4day = fResponse.list[24].dt_txt;
+    var day5day = fResponse.list[32].dt_txt;
+    var weatherIcon1 = fResponse.list[0].weather[0].icon;
+    var weatherIcon2 = fResponse.list[8].weather[0].icon;
+    var weatherIcon3 = fResponse.list[16].weather[0].icon;
+    var weatherIcon4 = fResponse.list[24].weather[0].icon;
+    var weatherIcon5 = fResponse.list[32].weather[0].icon;
+    var day1temp = fResponse.list[0].main.temp;
+    var day2temp = fResponse.list[8].main.temp;
+    var day3temp = fResponse.list[16].main.temp;
+    var day4temp = fResponse.list[24].main.temp;
+    var day5temp = fResponse.list[32].main.temp;
+    var day1humidity = fResponse.list[0].main.humidity;
+    var day2humidity = fResponse.list[8].main.humidity;
+    var day3humidity = fResponse.list[16].main.humidity;
+    var day4humidity = fResponse.list[24].main.humidity;
+    var day5humidity = fResponse.list[32].main.humidity;
+    
+    $('#day1day').html(day1day);
+    $('#day2day').html(day2day);
+    $('#day3day').html(day3day);
+    $('#day4day').html(day4day);
+    $('#day5day').html(day5day);
+
+    $('#day1temp').html(`Temperature: ${day1temp} \xB0F`);
+    $('#day2temp').html(`Temperature: ${day2temp} \xB0F`);
+    $('#day3temp').html(`Temperature: ${day3temp} \xB0F`);
+    $('#day4temp').html(`Temperature: ${day4temp} \xB0F`);
+    $('#day5temp').html(`Temperature: ${day5temp} \xB0F`);
+
+    $('#day1humidity').html(`Humidty: ${day1humidity} %`)
+    $('#day2humidity').html(`Humidty: ${day2humidity} %`)
+    $('#day3humidity').html(`Humidty: ${day3humidity} %`)
+    $('#day4humidity').html(`Humidty: ${day4humidity} %`)
+    $('#day5humidity').html(`Humidty: ${day5humidity} %`)
+
+    // var img1 = $('<img>');
+    // img1.attr('src', weatherIcon1, iconURL);
+    // $('#weatherIcon1').append(img1);
+
 })
 })
